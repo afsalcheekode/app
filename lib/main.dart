d3de8d03d1c72992bfd6dc9cca89d2584934ab5e
wo6k b00k <wo6kbook@gmail.com>
2026-06-02 14:58:55 +0530

    Fix missing import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'data_store.dart';
 import 'login_screen.dart';
