564bbd2fc1d1ea2a7dcce2c72cff689d67a29675
wo6k b00k <wo6kbook@gmail.com>
2026-06-05 11:09:49 +0530

    Replace STUDIO text with app_name_arabic image in student board

--- lib/student_board.dart
+++ lib/student_board.dart
@@ -183,7 +183,7 @@ class _StudentBoardScreenState extends State<StudentBoardScreen> with NoticeCent
         centerTitle: true,
         title: Column(
           children: [
-            const Text('STUDIO', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            Image.asset('assets/images/app_name_arabic.png', height: 24, color: Colors.white),
             Text('Class ${widget.studentClass}', style: TextStyle(fontSize: 10, color: Colors.white.withOpacity(0.8))),
           ],
         ),
